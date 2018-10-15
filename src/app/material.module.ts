ae3fd414462b05e712661c41f3f3eab69919dfaf
Bruno Krebs <bruno.krebs@auth0.com>
2018-10-15 13:58:22 -0300

    starting up

---
+++ src/app/material.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+
+import {
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatListModule,
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+  ]
+})
+export class MaterialModule {}
